e2564045e9dfd5e68922782a11427d45a4b9a20a
nana-kn <knyazewa2015@mail.ru>
2022-03-13 16:04:32 +0300

    Внесены изменения в тестовый скрипт

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ def test_guest_should_see_button_to_add(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_elements_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert button is not None, "No create class button"
+    assert button is not None, "No button to add item to cart"
     
